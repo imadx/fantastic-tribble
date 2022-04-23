302ac5179f9d801809c8134dc62a87da03b5ad06
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-04-23 19:25:00 +0530

    remove redundant code for handling upvotes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,43 +9,6 @@ window.addEventListener('DOMContentLoaded', () => {
     );
   });
 
-  document.querySelectorAll('.action-upvote').forEach((item) => {
-    const { threadId } = item.dataset;
-    if (!threadId) {
-      return;
-    }
-
-    item.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      const element = document.querySelector(
-        `.action-upvote[data-thread-id="${threadId}"]`
-      );
-      if (element.dataset.upvotes === '-') {
-        element.dataset.upvotes = '0';
-      }
-
-      element.dataset.upvotes = Number(element.dataset.upvotes) + 1;
-
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', `/thread/${threadId}/upvote`, true);
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xhr.onreadystatechange = function () {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
-          let response = this.response;
-          if (typeof response === 'string') {
-            response = JSON.parse(response);
-          }
-
-          element.dataset.upvotes = response.count;
-        } else {
-          element.dataset.upvotes = '-';
-        }
-      };
-      xhr.send(null);
-    });
-  });
-
   document.querySelectorAll('.action-reply').forEach((item) => {
     const { threadId } = item.dataset;
     if (!threadId) {
