1aa61424223a9e7e0a2d98f385871a85a2467804
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-04-23 19:09:46 +0530

    reset migrations

--- prisma/migrations/20220423131318_update_models/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Thread" DROP CONSTRAINT "Thread_parentThreadId_fkey";
-
--- AlterTable
-ALTER TABLE "Thread" ALTER COLUMN "parentThreadId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Thread" ADD CONSTRAINT "Thread_parentThreadId_fkey" FOREIGN KEY ("parentThreadId") REFERENCES "Thread"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20220423133903_reset_migrations/migration.sql
+++ prisma/migrations/20220423133903_reset_migrations/migration.sql
@@ -6,17 +6,9 @@ CREATE TABLE "Thread" (
     "authorId" TEXT NOT NULL,
     "message" TEXT NOT NULL,
     "upvotes" INTEGER NOT NULL DEFAULT 0,
-    "parentThreadId" TEXT NOT NULL,
+    "parentThreadId" TEXT,
 
     CONSTRAINT "Thread_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Reply" (
-    "id" TEXT NOT NULL,
-    "threadId" TEXT,
-
-    CONSTRAINT "Reply_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -32,16 +24,10 @@ CREATE TABLE "Author" (
 CREATE UNIQUE INDEX "Thread_id_key" ON "Thread"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Reply_id_key" ON "Reply"("id");
-
--- CreateIndex
 CREATE UNIQUE INDEX "Author_id_key" ON "Author"("id");
 
 -- AddForeignKey
-ALTER TABLE "Thread" ADD CONSTRAINT "Thread_parentThreadId_fkey" FOREIGN KEY ("parentThreadId") REFERENCES "Thread"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Thread" ADD CONSTRAINT "Thread_parentThreadId_fkey" FOREIGN KEY ("parentThreadId") REFERENCES "Thread"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Thread" ADD CONSTRAINT "Thread_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Author"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Reply" ADD CONSTRAINT "Reply_threadId_fkey" FOREIGN KEY ("threadId") REFERENCES "Thread"("id") ON DELETE SET NULL ON UPDATE CASCADE;
