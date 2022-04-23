3128d4dfb60a9f04b32be5e662a821581182188e
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-04-23 13:36:29 +0530

    add post and upvote flows

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,4 +8,33 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     );
   });
+
+  document.querySelectorAll('.action-upvote').forEach((item) => {
+    const { threadId } = item.dataset;
+    if (!threadId) {
+      return;
+    }
+
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const element = document.querySelector(`[data-thread-id="${threadId}"]`);
+      element.dataset.upvotes = Number(element.dataset.upvotes) + 1;
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', `/thread/${threadId}/upvote`, true);
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.onreadystatechange = function () {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
+          let response = this.response;
+          if (typeof response === 'string') {
+            response = JSON.parse(response);
+          }
+
+          element.dataset.upvotes = response.count;
+        }
+      };
+      xhr.send(null);
+    });
+  });
 });
