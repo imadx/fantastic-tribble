26effbccaca18bc28b4c53711f0ef997cb87fd70
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-04-23 16:08:37 +0530

    update deployment script to run migrations

---
+++ prisma/migrations/20220423103204_initialize_models/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "Thread" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "authorId" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "upvotes" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Thread_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Reply" (
+    "id" TEXT NOT NULL,
+    "threadId" TEXT,
+
+    CONSTRAINT "Reply_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Author" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "photoURL" TEXT NOT NULL,
+
+    CONSTRAINT "Author_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Thread_id_key" ON "Thread"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Reply_id_key" ON "Reply"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Author_id_key" ON "Author"("id");
+
+-- AddForeignKey
+ALTER TABLE "Thread" ADD CONSTRAINT "Thread_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Author"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reply" ADD CONSTRAINT "Reply_threadId_fkey" FOREIGN KEY ("threadId") REFERENCES "Thread"("id") ON DELETE SET NULL ON UPDATE CASCADE;
